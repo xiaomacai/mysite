e4d4091d910ce9c0035f3c8a14a00b6e35dac3d2
xiaomacai <xmc1028@gmail.com>
2018-01-16 18:07:40 +0800

    init

---
+++ blog/migrations/0001_initial.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+import datetime
+from django.utils.timezone import utc
+from django.conf import settings
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        migrations.swappable_dependency(settings.AUTH_USER_MODEL),
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Post',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', primary_key=True, serialize=False, auto_created=True)),
+                ('title', models.CharField(max_length=250)),
+                ('slug', models.SlugField(max_length=250, unique_for_date='publish')),
+                ('body', models.TextField()),
+                ('publish', models.DateTimeField(default=datetime.datetime(2018, 1, 16, 1, 54, 18, 15461, tzinfo=utc))),
+                ('created', models.DateTimeField(auto_now_add=True)),
+                ('updated', models.DateTimeField(auto_now=True)),
+                ('status', models.CharField(max_length=10, default='draft', choices=[('draft', 'draft'), ('published', 'published')])),
+                ('author', models.ForeignKey(related_name='blog_posts', to=settings.AUTH_USER_MODEL)),
+            ],
+            options={
+                'ordering': ('-publish',),
+            },
+        ),
+    ]
